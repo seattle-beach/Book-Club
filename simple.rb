53acfb61674e83c6e1282cbb9b6aec06f7ef3793
Alpha Chen <alpha.chen@gmail.com>
2014-05-06 18:09:30 -0700

    Variables!

--- simple.rb
+++ simple.rb
@@ -27,11 +27,11 @@ class Add < Struct.new(:left, :right)
     true
   end
 
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      Add.new(left.reduce, right)
+      Add.new(left.reduce(environment), right)
     elsif right.reducible?
-      Add.new(left, right.reduce)
+      Add.new(left, right.reduce(environment))
     else
       Number.new(left.value + right.value)
     end
@@ -51,11 +51,11 @@ class Multiply < Struct.new(:left, :right)
     true
   end
 
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      Multiply.new(left.reduce, right)
+      Multiply.new(left.reduce(environment), right)
     elsif right.reducible?
-      Multiply.new(left, right.reduce)
+      Multiply.new(left, right.reduce(environment))
     else
       Number.new(left.value * right.value)
     end
@@ -89,20 +89,38 @@ class LessThan < Struct.new(:left, :right)
     true
   end
 
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      LessThan.new(left.reduce, right)
+      LessThan.new(left.reduce(environment), right)
     elsif right.reducible?
-      LessThan.new(left, right.reduce)
+      LessThan.new(left, right.reduce(environment))
     else
       Boolean.new(left.value < right.value)
     end
   end
 end
 
-class Machine < Struct.new(:expression)
+class Variable < Struct.new(:name)
+  def to_s
+    name.to_s
+  end
+
+  def inspect
+    "«#{self}»"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    environment[name]
+  end
+end
+
+class Machine < Struct.new(:expression, :environment)
   def step
-    self.expression = expression.reduce
+    self.expression = expression.reduce(environment)
   end
 
   def run
@@ -116,6 +134,6 @@ class Machine < Struct.new(:expression)
 end
 
 machine = Machine.new(
-  LessThan.new(Number.new(5), Add.new(Number.new(2), Number.new(2)))
+  Add.new(Variable.new(:x), Variable.new(:y)), { x: Number.new(3), y: Number.new(4) }
 )
 machine.run
