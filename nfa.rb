9127b3aa2bf3a58fa29e796e90b4252c1eef2ef8
Alpha Chen <alpha.chen@gmail.com>
2014-09-30 20:06:48 -0700

    Free moves

--- nfa.rb
+++ nfa.rb
@@ -14,6 +14,16 @@ class NFARulebook < Struct.new(:rules)
   def rules_for(state, character)
     rules.select {|rule| rule.applies_to?(state, character) }
   end
+
+  def follow_free_moves(states)
+    more_states = next_states(states, nil)
+
+    if more_states.subset?(states)
+      states
+    else
+      follow_free_moves(states + more_states)
+    end
+  end
 end
 
 class NFA < Struct.new(:current_states, :accept_states, :rulebook)
@@ -30,6 +40,10 @@ class NFA < Struct.new(:current_states, :accept_states, :rulebook)
       read_character(character)
     end
   end
+
+  def current_states
+    rulebook.follow_free_moves(super)
+  end
 end
 
 class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
@@ -40,4 +54,24 @@ class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
   def to_nfa
     NFA.new(Set[start_state], accept_states, rulebook)
   end
+end
+
+if __FILE__ == $0
+  rulebook = NFARulebook.new([
+    FARule.new(1, nil, 2), FARule.new(1, nil, 4),
+    FARule.new(2, 'a', 3),
+    FARule.new(3, 'a', 2),
+    FARule.new(4, 'a', 5),
+    FARule.new(5, 'a', 6),
+    FARule.new(6, 'a', 4)
+  ])
+
+  p rulebook.next_states(Set[1], nil)
+  p rulebook.follow_free_moves(Set[1])
+
+  nfa_design = NFADesign.new(1, [2, 4], rulebook)
+  p nfa_design.accepts?('aa')
+  p nfa_design.accepts?('aaa')
+  p nfa_design.accepts?('aaaaa')
+  p nfa_design.accepts?('aaaaaa')
 end
