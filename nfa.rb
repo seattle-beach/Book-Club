b468ad310f22e870e940ba8e74378a4d74700d04
Alpha Chen <alpha.chen@gmail.com>
2014-10-01 17:44:21 -0700

    NFASimulation

--- nfa.rb
+++ nfa.rb
@@ -1,6 +1,7 @@
 require 'set'
 
 require_relative 'fa'
+require_relative 'dfa'
 
 class NFARulebook < Struct.new(:rules)
   def next_states(states, character)
@@ -23,6 +24,10 @@ class NFARulebook < Struct.new(:rules)
     else
       follow_free_moves(states + more_states)
     end
+  end
+
+  def alphabet
+    rules.map(&:character).compact.uniq
   end
 end
 
@@ -51,7 +56,41 @@ class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
     to_nfa.tap {|nfa| nfa.read_string(string) }.accepting?
   end
 
-  def to_nfa
-    NFA.new(Set[start_state], accept_states, rulebook)
+  def to_nfa(current_states=nil)
+    current_states ||= Set[start_state]
+    NFA.new(current_states, accept_states, rulebook)
+  end
+end
+
+class NFASimulation < Struct.new(:nfa_design)
+  def next_state(state, character)
+    nfa = nfa_design.to_nfa(state)
+    nfa.read_character(character)
+    nfa.current_states
+  end
+
+  def rules_for(state)
+    nfa_design.rulebook.alphabet.map do |character|
+      FARule.new(state, character, next_state(state, character))
+    end
+  end
+
+  def discover_states_and_rules(states)
+    rules = states.flat_map {|state| rules_for(state) }
+    more_states = rules.map(&:follow).to_set
+
+    if more_states.subset?(states)
+      [states, rules]
+    else
+      discover_states_and_rules(states + more_states)
+    end
+  end
+
+  def to_dfa_design
+    start_state = nfa_design.to_nfa.current_states
+    states, rules = discover_states_and_rules(Set[start_state])
+    accept_states = states.select {|state| nfa_design.to_nfa(state).accepting? }
+
+    DFADesign.new(start_state, accept_states, DFARulebook.new(rules))
   end
 end
