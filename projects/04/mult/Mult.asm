656c2d54424befb4f0c2ba092c347b95d815541a
Alpha Chen <alpha.chen@gmail.com>
2016-05-14 10:11:51 -0700

    [04] Mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,32 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// r3 = r0
+  @0
+  D=M
+  @3
+  M=D
+// r2 = 0
+  @2
+  M=0
+// while {
+(LOOP)
+//   break if r3 == 0
+  @3
+  D=M
+  @END
+  D;JEQ
+//   r2 += r1
+  @2
+  D=M
+  @1
+  D=D+M
+  @2
+  M=D
+//   r3 -= 1
+  @3
+  M=M-1
+// }
+  @LOOP
+  0;JMP
+(END)
