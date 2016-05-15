88a673a725b9d5f4915bc4287ce756ee715b17c4
Alpha Chen <alpha.chen@gmail.com>
2016-05-15 10:16:17 -0700

    [04] Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -8,4 +8,44 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+// r0 = screen
+  @SCREEN
+  D=A
+  @R0
+  M=D
+// while {
+(LOOP)
+//   if kbd == 0 { // no key is pressed
+  @KBD
+  D=M
+  @NOKEY
+  D;JNE
+//     r1 = 0
+  @R1
+  M=0
+  @ENDIF
+  0;JMP
+//   } else { // a key is pressed
+(NOKEY)
+//     r1 = 1
+  @R1
+  M=-1
+//   }
+(ENDIF)
+//  *r0 = r1
+  @R1
+  D=M
+  @R0
+  A=M
+  M=D
+//   r0 += 1
+  @R0
+  M=M+1
+//   r0 &= !32
+  @8192
+  D=!A
+  @R0
+  M=M&D
+// }
+  @LOOP
+  0;JMP
