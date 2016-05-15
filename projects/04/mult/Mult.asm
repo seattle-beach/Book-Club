a5454ea1c5aa07c84a5901085d07ace966821f38
Alpha Chen <alpha.chen@gmail.com>
2016-05-15 10:14:40 -0700

    [04] Use register symbols in Mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,27 +7,27 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // r3 = r0
-  @0
+  @R0
   D=M
-  @3
+  @R3
   M=D
 // r2 = 0
-  @2
+  @R2
   M=0
 // while {
 (LOOP)
 //   break if r3 == 0
-  @3
+  @R3
   D=M
   @END
   D;JEQ
 //   r2 += r1
-  @1
+  @R1
   D=M
-  @2
+  @R2
   M=M+D
 //   r3 -= 1
-  @3
+  @R3
   M=M-1
 // }
   @LOOP
